bdbe29cba91296c8e5f4efb1d0d787d6da12f17e
zilaei <zzilaei@gmail.com>
2023-07-10 16:31:52 +0200

    feat(faq-parser): parser for Hbg planning permission FAQ
    gitignore folder output

---
+++ tools/faq_parser/faq-parser.py
@@ -0,0 +1,51 @@
+import requests
+from bs4 import BeautifulSoup
+import html2text
+import argparse
+import time
+
+# setup command line arguments
+parser = argparse.ArgumentParser(description='Convert Hbg planning permission FAQ to Markdown')
+parser.add_argument('--url', type=str, help='The URL to parse')
+parser.add_argument('--class_value', type=str, help='The HTML class containing the FAQ')
+parser.add_argument('--output_file', type=str, default='output.md', help='The output Markdown file name')
+
+args = parser.parse_args()
+
+url = args.url
+class_value = args.class_value
+output_file = args.output_file
+
+# parse the HTML
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
+
+# we only want the accordions containing the FAQ
+element = soup.find('div', {'class': class_value})
+
+# convert the HTML to Markdown
+h = html2text.HTML2Text()
+h.ignore_links = True       # remove all hyperlinks from text
+
+markdown = h.handle(element.prettify())
+
+# post-process the markdown to convert accordions to h1 headers
+# the accordions are converted to '<some title>  keyboard_arrow_down'
+# we want to replace accordians with '# <some title>'
+lines = markdown.split('\n')
+processed_lines = []
+
+for line in lines:
+    if line.endswith('keyboard_arrow_down'):
+        processed_lines.append('# ' + line.replace('keyboard_arrow_down', ''))
+    else:
+        processed_lines.append(line)
+
+processed_markdown = '\n'.join(processed_lines)
+
+# save the markdown to a file
+# filename can be set by the user, default is output.md
+with open(output_file, 'w') as f:
+    f.write(processed_markdown)
+
+print(f"Markdown file saved as {output_file}")
